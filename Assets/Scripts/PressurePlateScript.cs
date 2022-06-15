6521f11e939f1f377ffb785dc21adc8fded3715b
Sar-rgb <sarah.abbas@hotmail.se>
2022-06-15 12:35:33 +0200

    Primitiv PressurePlate
    Nu finns det ett objekt i scenen som får olika färg beroende på om man står på det eller inte!

---
+++ Assets/Scripts/PressurePlateScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateScript : MonoBehaviour
+{
+
+    [SerializeField] private Color defaultColor;
+    [SerializeField] private Color pressedColor;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = defaultColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = pressedColor;
+        Debug.Log("pressed!");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = defaultColor;
+        Debug.Log("unpressed!");
+    }
+}
