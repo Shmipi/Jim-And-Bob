2da14a99a6f7f82dfa53bd9fba418368eac51888
Ludvig Burud <ludvigburud@gmail.com>
2022-06-19 01:49:20 +0200

    Menu + work on audio

--- Assets/Scripts/MenuButtonScript.cs
+++ Assets/Scripts/MenuButtonScript.cs
@@ -5,14 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonScript : MonoBehaviour
 {
+
+    [SerializeField] GameObject LevelSelectPane;
+    [SerializeField] GameObject ButtonPane;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void PlaySandbox()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LevelSelect()
+    {
+        LevelSelectPane.SetActive(true);
+        ButtonPane.SetActive(false);
+    }
+
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void BackButton()
+    {
+        LevelSelectPane.SetActive(false);
+        ButtonPane.SetActive(true);
     }
 
     public void QuitGame()
